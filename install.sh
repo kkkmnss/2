93b47196508f5e865223f9948db18bfba59daed9
cliugmail <153306173+kkkmnss@users.noreply.github.com>
2024-10-17 19:49:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-# 确保脚本在遇到错误时停止执行
-set -e
 
-# 更新和安装必需的包
-pkg update -y && pkg upgrade -y
-pkg install python git clang cmake build-essential ninja -y
+# 更新包列表和已安装的包
+pkg update && pkg upgrade -y
 
-# 创建并激活Python虚拟环境
-python -m venv venv
+# 安装编译工具和其他必要软件
+pkg install -y clang python cmake make libffi libtool autoconf automake patchelf git wget curl
+
+# 设置Python虚拟环境
+pip install --upgrade pip
+pip install virtualenv
+virtualenv venv
 source venv/bin/activate
 
-# 安装Python依赖
-pip install --default-timeout=300 -i https://pypi.tuna.tsinghua.edu.cn/simple implib requests pysocks cfscrape scapy setuptools
+# 安装Python库
+pip install numpy requests
 
-# 克隆项目并安装项目依赖
-git clone https://github.com/kkkmnss/1.git/
-cd 1
-CMAKE_GENERATOR="Unix Makefiles" pip install --default-timeout=300 -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
-CMAKE_GENERATOR="Unix Makefiles" pip install --default-timeout=300 -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade requests urllib3 charset_normalizer
+# 克隆你的GitHub项目
+git clone https://github.com/kkkmnss/1.git
+cd 2
 
-echo "安装完成！"
+# 根据你的项目需要执行的命令
+# 如果有具体的构建或安装步骤，可以在这里指定，例如：
+# ./configure
+# make
+# make install
+
+echo "安装完成！虚拟环境已激活，可以开始工作。"
