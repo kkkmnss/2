0547572d91da051ce6e37a612b576cb212af5ad4
cliugmail <153306173+kkkmnss@users.noreply.github.com>
2024-10-18 16:57:55 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,15 +12,10 @@ export DEBIAN_FRONTEND=noninteractive
 # 更新软件包列表
 echo "正在更新软件包列表..."
 pkg update -y
-pkg install openssl-tool
 
 # 安装必要的软件和 Rust 编译器
 echo "正在安装 Python、Clang、OpenSSL、Git 和 Rust..."
 pkg install python clang openssl git rust binutils -y
-
-# 检查 ar 工具是否安装
-echo "检查 ar 工具是否已安装..."
-which aarch64-linux-android-ar || echo "ar 工具未安装，请检查您的安装步骤！"
 
 # 克隆包含 .whl 文件的 GitHub 仓库
 echo "正在从 GitHub 克隆 wh 仓库..."
