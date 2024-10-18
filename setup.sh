478f083a44386d6912d22f5dc275fef15cf5b97e
cliugmail <153306173+kkkmnss@users.noreply.github.com>
2024-10-18 16:30:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,19 +8,11 @@ fi
 
 # 更新软件包列表
 echo "正在更新软件包列表..."
-pkg update && pkg upgrade -y
+DEBIAN_FRONTEND=noninteractive pkg update -y
 
-# 安装必要的软件
-echo "正在安装 Python、Clang、OpenSSL、Git、Rust..."
-pkg install python clang openssl git rust -y
-
-# 安装Python的pip和开发工具
-pkg install python-pip python-dev build-essential -y
-
-# 克隆 MHDDOS 项目
-echo "正在从 GitHub 克隆 MHDDOS 项目..."
-git clone https://github.com/kkkmnss/1.git
-cd 1
+# 安装必要的软件和 Rust 编译器
+echo "正在安装 Python、Clang、OpenSSL、Git、Rust 和 OpenSSL 开发库..."
+DEBIAN_FRONTEND=noninteractive pkg install python clang openssl git rust openssl-tool openssl-dev -y --assume-yes
 
 # 克隆包含 .whl 文件的 GitHub 仓库
 echo "正在从 GitHub 克隆 wh 仓库..."
@@ -33,4 +25,10 @@ for wheel in *.whl; do
     pip install "$wheel"
 done
 
+# 克隆 MHDDOS 项目
+echo "正在从 GitHub 克隆 MHDDOS 项目..."
+git clone https://github.com/kkkmnss/1.git
+cd 1
+
+# 环境设置完成，可以开始项目工作
 echo "环境设置完成，现在你可以开始你的项目工作了。"
